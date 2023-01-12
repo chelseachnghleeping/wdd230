b04878403a4286c07c81a6a62ce2d27309d8e793
chelsea chngh lee ping <chn22001@byui.edu>
2023-01-12 23:10:43 +0800

    removed files

--- date_modified.js
+++
@@ -1,5 +0,0 @@
-var string = document.lastModified;
-let LastModified = new Date(document.lastModified);
-let day = (LastModified.getMonth() + 1) + "/" + LastModified.getDate() + "/" + LastModified.getFullYear();
-let hour = LastModified.getHours() + ":" + LastModified.getMinutes() + ":" + LastModified.getSeconds();
-document.getElementById("LastModified").innerHTML = day + " " + hour;
